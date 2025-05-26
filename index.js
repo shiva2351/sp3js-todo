b5742b39bb4c390904a3ddbba943d1e28fad8661
shiva <smpgr2351@gmail.com>
2025-05-26 18:42:17 +0530

    second

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const add=document.getElementById("add")
 const list=document.getElementById("todo-list")
-const item=document.createElement("li")
 const text=document.getElementById("text")
+const tasks=document.getElementById("tasks")
+let id=0;
 add.addEventListener("click",(event)=>{
-    console.log(text.value)
-    item.textContent=text.value
-    list.appendChild(item)
+    id+=1
+    let i=document.createElement("li")
+    i.textContent=text.value
+    i.key=id
+    list.appendChild(i)
+    tasks.textContent=id
 })
